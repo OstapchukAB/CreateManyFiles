e8a0deba75cf3673aac28d13067e7172ba364ef4
OstapchukAB <electron.anton@gmail.com>
2024-12-24 14:13:46 +0300

    поправил формат порядкового номера

--- Program.cs
+++ Program.cs
@@ -29,11 +29,11 @@ class Program
             // Формируем имя файла: метка времени + порядковый номер
             // Пример: 20241221_104530_123_0.txt
             string timestamp = DateTime.Now.ToString("yyyyMMdd_HHmmss_fff");
-            string fileName = $"{timestamp}_{i}.txt";
+            string fileName = $"{i:00000}_{timestamp}.txt";
             string fullPath = Path.Combine(directoryPath, fileName);
 
             // Создаем файл и записываем внутрь порядковый номер
-            File.WriteAllText(fullPath, i.ToString());
+            File.WriteAllText(fullPath, i.ToString("00000"));
         });
 
         sw.Stop();
